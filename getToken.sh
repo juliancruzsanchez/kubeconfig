e91aa8594c147e70e22ecb124e04a4f22cf6b4f9
Julian Cruz Sanchez <kickbutt.js@gmail.com>
2021-01-26 12:29:58 -0700

    Delete getToken.sh

--- getToken.sh
+++
@@ -1,1 +0,0 @@
-kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}" | curl -F 'f:1=<-' ix.io
