99ac195e0488b3affa1b11ddb3206c857091eef7
Julian Cruz Sanchez <kickbutt.js@gmail.com>
2021-01-26 13:11:30 -0700

    Update index.sh

--- index.sh
+++ index.sh
@@ -2,22 +2,42 @@
 
 repo="juliancruzsanchez/kubeconfig"
 username="admin-user"
-while getopts ixio:username:u:help:tokenOnly: flag
-do
-    case "${flag}" in
-        ixio) ixio=${OPTARG};;
-        help) help=true
-        username) username=${OPTARG};;
-        u) username=${OPTARG};;
-        tokenOnly) tokenOnly=${OPTARG};;
+ixio="f"
+tokenOnly="f"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        i | ixio) shift
+                    ixio=t
+                    ;;
+        h | help) 
+                    showHelp
+                    exit
+                    ;;
+        u | username) 
+                    username=$1
+                    ;;
+        tokenOnly | t) 
+                    tokenOnly=t
+                    ;;
     esac
+    shift
 done
 
 mkdir .kui_init
 cd .kui_init
 
+function showHelp {
+  echo """
+  i | ixio - Pipes token to ix.io then displays link (useful for systems that're RDPed into CLIs)
+  h | help - Shows this menu
+  u | username [username] - Set username for user
+  t | tokenOnly - Only generates token doesn't make user
+  """
+}
 # Makes Users
 function makeUsers {
+  echo "Making User $username..."
   cat <<EOF | kubectl apply -f -
   apiVersion: v1
   kind: ServiceAccount
@@ -25,12 +45,16 @@ function makeUsers {
     name: $username
     namespace: kubernetes-dashboard
   EOF
-
+  
+  
+  echo "Done."
+  echo "Assigning ClusterRole to $username..."
+  
   cat <<EOF | kubectl apply -f -
   apiVersion: rbac.authorization.k8s.io/v1
   kind: ClusterRoleBinding
   metadata:
-    name: admin-user
+    name: $username
   roleRef:
     apiGroup: rbac.authorization.k8s.io
     kind: ClusterRole
@@ -45,13 +69,17 @@ function makeUsers {
 function getToken {
   kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/$username -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}" 1> token
   cat token
-  if [ $ixio = true ]; then
+  if [ $ixio = "t" ]; then
     cat token | curl -F 'f:1=<-' ix.io
   fi
 }
 
-if [$tokenOnly = true]; then
+if [$tokenOnly = "t"]; then
   getToken
 else
   makeUsers
   getToken
+fi
+
+cd ..
+rm -rf .kui_init
